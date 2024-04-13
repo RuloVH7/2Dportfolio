bc6a3f48432f23ed1646eb55bce69412c10d8636
RuloVH7 <rulo_van_halen@hotmail.com>
2024-04-12 23:35:07 -0600

    Update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // Import Kaboom instance from the file we created
+import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx"
 
 // Load an image as a sprite with the method loadSprite
@@ -16,3 +17,22 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
        "walk-up":{from: 1014, to:1017, loop: true, speed: 8},
     },
 });
+
+k.loadSprite("map", "./map.png");
+
+//Create a background
+k.setBackground(k.Color.fromHex("#311047"))
+
+//Create a scene
+k.scene("main", async () => {
+    const mapData = await (await fetch("./map.json")).json()
+    const layers = mapData.layers;
+
+    const map = k.make([
+        k.sprite("map"),
+        k.pos(0),
+        k.scale(scaleFactor)
+    ])
+});
+
+k.go("main");
